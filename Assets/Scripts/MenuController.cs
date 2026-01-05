39042f8d2394ba18144e2d684244e073e6609cfc
HoangPhuong2912 <mhoangphuong202@gmail.com>
2026-01-05 09:50:45 +0700

    first commit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public static int activeLevel;
+    public static bool isPlay;
+    [SerializeField] GameObject gamePanel, level, winPanel, losePanel, menuPanel;
+    [SerializeField] GameObject settingPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (PlayerPrefs.HasKey("activeLevel") == true) { activeLevel = PlayerPrefs.GetInt("activeLevel"); }
+        else activeLevel = 0;
+
+        if (isPlay == true)
+        {
+            StartGame();
+        }
+    }
+
+    // Update is called once per frame
+    public void StartGame()
+    {
+        menuPanel.SetActive(false);
+        gamePanel.SetActive(true);
+        level.SetActive(true);
+    }
+
+
+
+
+    public void WinPanel()
+    {
+        winPanel.SetActive(true);
+    }
+
+    public void NextLevel()
+    {
+        AdsController.Instance.ShowTransition();
+        MenuController.isPlay = true;
+        activeLevel++;
+        SceneManager.LoadScene(0);
+        PlayerPrefs.SetInt("activeLevel", activeLevel);
+        settingPanel.SetActive(false);
+    }
+
+    public void Reload()
+    {
+        MenuController.isPlay = true;
+        SceneManager.LoadScene(0);
+        PlayerPrefs.SetInt("activeLevel", activeLevel);
+
+    }
+
+    public void LevelSkip()
+    {
+
+        if (AdsController.Instance.ShowReward(RewardState.AddMoney))
+        {
+            settingPanel.SetActive(false);
+            WinPanel();
+        }
+        else
+        {
+            Debug.Log("Fail Skip Level");
+        }
+    }
+
+
+    public void MainMenu()
+    {
+        menuPanel.SetActive(true);
+        gamePanel.SetActive(false);
+        level.SetActive(false);
+        winPanel.SetActive(false);
+        losePanel.SetActive(false);
+        settingPanel.SetActive(false);
+    }
+
+
+    public void CloseMenu(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    public void OpenMenu(GameObject obj)
+    {
+        obj.SetActive(true);
+
+    }
+}
